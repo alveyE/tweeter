f4af6e1e08a3ff24e86b9e51363b047fe944a532
alveyE <gcbdalvey@gmail.com>
2025-02-04 10:04:49 -0700

    first commit

---
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Status } from "tweeter-shared";
+import Post from "./Post";
+
+interface StatusItemProps {
+  status: Status;
+}
+
+const StatusItem: React.FC<StatusItemProps> = ({ status }) => {
+  return (
+    <div className="row mb-3 mx-0 px-0 border rounded bg-white">
+      <div className="col bg-light mx-0 px-0">
+        <div className="container px-0">
+          <div className="row mx-0 px-0">
+            <div className="col-auto p-3">
+              <img
+                src={status.user.imageUrl}
+                className="img-fluid"
+                width="80"
+                alt="Posting user"
+              />
+            </div>
+            <div className="col">
+              <h2>
+                <b>
+                  {status.user.firstName} {status.user.lastName}
+                </b>{" "}
+                -{" "}
+                <Link
+                  to={status.user.alias}
+                  onClick={(event) => navigateToUser(event)}
+                >
+                  {status.user.alias}
+                </Link>
+              </h2>
+              {status.formattedDate}
+              <br />
+              <Post status={status} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatusItem;
